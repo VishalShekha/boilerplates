234b005d9ff4f9bb6ed03e06086fac399753d2ee
Vishal Shekha <vishal.tshekha@gmail.com>
2024-08-10 11:30:03 +0530

    Pyramid

---
+++ pattern/centerPyramid.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int printP(int n){
+    for(int i =0; i<n;i++){
+        //space
+        for(int j=0; j<n-i+1;j++){
+            printf(" ");
+        }
+        //star
+        for(int j=0; j<(2*i)+1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    printP(n);
+    return 0;
+}
